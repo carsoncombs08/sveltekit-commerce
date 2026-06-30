05c62bef94761f9025943013a8415ee403499261
carsoncombs08 <carsoncombs80@gmail.com>
2026-06-30 16:15:51 -0400

    Revert "Add About Us page with contact section and related footer links/icons"
    This reverts commit 51bd0dd26cfccb05fb9fac5a0b4d6382c8a8e8dd.

--- src/components/Icons.svelte
+++ src/components/Icons.svelte
@@ -131,55 +131,5 @@
       shape-rendering="geometricPrecision"
       class="Collapse_icon__JsuEg"><path d="M9 18l6-6-6-6" /></svg
     >
-  {:else if type === 'phone'}
-    <svg
-      style={`width:${width};height:${height}`}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke={strokeColor}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      ><path
-        d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"
-      /></svg
-    >
-  {:else if type === 'mail'}
-    <svg
-      style={`width:${width};height:${height}`}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke={strokeColor}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      ><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" /><path
-        d="M22 6l-10 7L2 6"
-      /></svg
-    >
-  {:else if type === 'copy'}
-    <svg
-      style={`width:${width};height:${height}`}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke={strokeColor}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      ><rect x="9" y="9" width="13" height="13" rx="2" ry="2" /><path
-        d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"
-      /></svg
-    >
-  {:else if type === 'check'}
-    <svg
-      style={`width:${width};height:${height}`}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke={strokeColor}
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      ><path d="M20 6L9 17L4 12" /></svg
-    >
   {/if}
 </div>
